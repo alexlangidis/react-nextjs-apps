03c2a8787d34b2fb28f74cefbbc44a73651434f2
alexlangidis <alexlag.dev@gmail.com>
2026-02-27 20:59:45 +0200

    Add rmtDev project to monorepo

---
+++ rmtDev/src/components/App.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import Background from "./Background";
+import Container from "./Container";
+import Footer from "./Footer";
+import Header, { HeaderTop } from "./Header";
+import Logo from "./Logo";
+import BookmarksButton from "./BookmarksButton";
+import SearchForm from "./SearchForm";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobItemContent from "./JobItemContent";
+import ResultsCount from "./ResultsCount";
+import JobList from "./JobList";
+import PaginationControls from "./PaginationControls";
+import SortingControls from "./SortingControls";
+import { JobItem } from "../lib/types";
+
+function App() {
+  const [searchText, setSearchText] = useState("");
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!searchText) {
+      setJobItems([]);
+      return;
+    }
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`,
+      );
+      const data = (await response.json()) as { jobItems: JobItem[] };
+      setIsLoading(false);
+      setJobItems(data.jobItems ?? []);
+    };
+
+    fetchData();
+  }, [searchText]);
+
+  return (
+    <>
+      <Background />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+          <JobList isLoading={isLoading} jobItems={jobItems} />
+          <PaginationControls />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
