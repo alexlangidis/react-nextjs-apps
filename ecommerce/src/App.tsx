5efcf3689b27c6a3b681c957f5b68dfcfe19308a
Alex Langidis <alexlag.dev@gmail.com>
2026-07-01 13:32:56 +0300

    push ecommerce

---
+++ ecommerce/src/App.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Link to={"/create"}>Home</Link>
+    </>
+  );
+}
+
+export default App;
